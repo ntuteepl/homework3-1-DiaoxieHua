be91a41254544a7e72492a38dfbe3e8520447d7f
DiaoxieHua <146065243+DiaoxieHua@users.noreply.github.com>
2023-10-18 16:24:42 +0800

    更新 hw3-3.c

--- hw3-3.c
+++ hw3-3.c
@@ -1,27 +1,27 @@
 #include <stdio.h>
 
-    int digitk(int n)
+int digitSum(int n) 
 {
-    int k=0;
-    while (n>0)
-{
-    k += n%10;
-    n /=10;
+    int k = 0;
+    while (n > 0) 
+    {
+        k += n % 10;
+        n /= 10;
     }
     return k;
 }
-    int main()
- {
+
+int main() 
+{
     int a, b;
-    scanf("%d %d", &a, &b); 
-    int kA = digitk(a);
-    int kB = digitk(b);
-     if (kA<kB||(kA==kB&&a<b)
-{
+    scanf("%d %d", &a, &b);
+    int kA =digitSum(a);
+    int kB =digitSum(b);
+    
+    if (kA<kB || (kA==kB && a<b)) {
         printf("%d", a);
-}
-else 
-{
+    } else 
+    {
         printf("%d", b);
-}
+    }
 }
