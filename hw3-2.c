8240c34650a4cb31484b2be0fb3f6347d11c0700
DiaoxieHua <146065243+DiaoxieHua@users.noreply.github.com>
2023-10-18 10:03:26 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+int part11,part12,part21,part22,part31,part32,cars;
+scanf("%d %d",&part11,&part12);
+scanf("%d %d",&part21,&part22);
+scanf("%d %d",&part31,&part32);
+cars=1;
+if(part11==part12||part21==part22||part31==part32){
+        printf("格式錯誤");
+}
+else{
+if(part12<=part21||part22<=part31||part12<=part22||part21<=part12)
+{
+    cars=cars+1;
+}
+else{
+        cars=1;
+
+}
+}
+printf("%d", cars);
+}
