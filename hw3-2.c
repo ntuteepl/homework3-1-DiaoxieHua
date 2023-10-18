5ab457462121e44363347cadcbbcfd6c0656a1e1
DiaoxieHua <146065243+DiaoxieHua@users.noreply.github.com>
2023-10-19 03:42:37 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -4,14 +4,14 @@ int main() {
     int a,b,c,d,e,f;
     scanf("%d %d %d %d %d %d",&a,&b,&c,&d,&e,&f);
     int L[3][2]={{a,b},{c,d},{e,f}};
-    
-    for (int i=0;i<3;i++) 
+
+    for (int i=0;i<3;i++)
 {
     for (int j=i+1;j<3;j++)
 {
     if (L[i][1]>L[j][1])
 {
-    int l[2] = {L[i][0],L[i][1]};
+    int l[2]={L[i][0],L[i][1]};
         L[i][0]=L[j][0];
         L[i][1]=L[j][1];
         L[j][0]=l[0];
@@ -20,16 +20,17 @@ int main() {
 }
 }
 int cars=1;
-int first back = L[0][1];
-    for (int i = 1; i < 3; i++) {
-    if (L[i][0] >= first back) {
-    first back = L[i][1];
+int back = L[0][1];
+    for (int i=1;i<3;i++)
+{
+    if (L[i][0]>=back)
+{
+    back=L[i][1];
 }
     else
 {
-    g++;
+    cars++;
 }
 }
-
-    printf("%d", cars);
+printf("%d", cars);
 }
