c2cf3f242b6fad67f56eb2347823de471566474b
DiaoxieHua <146065243+DiaoxieHua@users.noreply.github.com>
2023-10-18 10:03:34 +0800

    Update hw3-3.c

--- hw3-3.c
+++ hw3-3.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+int main(){
+int a, b;
+scanf("%d %d", &a, &b);
+int sa= 0;
+int sb= 0;
+int ta=a;
+int tb=b;
+    while ( ta>0 ){
+    sa+=ta%10;
+    ta/=10;
+}
+    while ( tb>0 ){
+    sb+=tb%10;
+    tb/=10;
+}
+    if (sa<sb||(sa==sb&&a<b)) {
+    printf("%d\n", a);
+}
+    else
+{
+    printf("%d\n", b);
+}
+}
